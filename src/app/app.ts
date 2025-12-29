019477f357215346b84818fb6b4b18f9f76e13f5
Slava Mihaelov <huskyslava@gmail.com>
2025-12-29 19:32:28 +0200

    Initial project setup, tsconfig, editorconfig

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, signal} from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.css",
 })
-export class App {
-  protected readonly title = signal('ng-rummi-q-client');
+export class App implements OnInit {
+	protected readonly title = signal("ng-rummi-q-client");
+
+	private test(x: any){
+		console.log(x)
+	}
+
+	ngOnInit() {
+		this.test("abc")
+	}
 }
