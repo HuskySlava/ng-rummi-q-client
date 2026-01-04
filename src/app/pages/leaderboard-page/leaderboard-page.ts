9522bfe3997ecb3f51b4dcc4df3639ac9cf63809
Slava Mihaelov <huskyslava@gmail.com>
2026-01-04 23:22:47 +0200

    Leaderboard page mock

--- src/app/pages/leaderboard-page/leaderboard-page.ts
+++ src/app/pages/leaderboard-page/leaderboard-page.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-leaderboard-page',
-  imports: [],
   templateUrl: './leaderboard-page.html',
   styleUrl: './leaderboard-page.css',
 })
 export class LeaderboardPage {
+	private router = inject( Router )
 
+	back() {
+		this.router.navigate([''])
+	}
 }
